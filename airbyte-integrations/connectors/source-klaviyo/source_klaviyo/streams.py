f7c6b79bcd0234343cb88a674b7df3260b6b380d
marionettists <marionettists@gmail.com>
2024-02-02 19:28:52 +0800

    feat: klaviyo flow messages

--- airbyte-integrations/connectors/source-klaviyo/source_klaviyo/streams.py
+++ airbyte-integrations/connectors/source-klaviyo/source_klaviyo/streams.py
@@ -182,7 +182,7 @@ class SemiIncrementalKlaviyoStream(KlaviyoStream, ABC):
         stream_state: Optional[Mapping[str, Any]] = None,
     ) -> Iterable[StreamData]:
         stream_state = stream_state or {}
-        starting_point = stream_state.get(self.cursor_field, self._start_ts)
+        starting_point = stream_state.get(self.cursor_field)
         for record in super().read_records(
             sync_mode=sync_mode, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state
         ):
@@ -377,6 +377,7 @@ class FlowFlowActions(HttpSubStream, IncrementalKlaviyoStream):
         self.logger.info("stream slice %s", json.dumps(stream_slice))
         for record in super().parse_response(response, **kwargs):
             record[self.parent_field] = stream_slice["parent"]["id"]
+            self.logger.info("record %s", json.dumps(record))
             yield record
 
 
@@ -405,6 +406,7 @@ class FlowActionMessages(HttpSubStream, IncrementalKlaviyoStream):
         self.logger.info("stream slice %s", json.dumps(stream_slice))
         for record in super().parse_response(response, **kwargs):
             record[self.parent_field] = stream_slice["parent"]["id"]
+            self.logger.info("record %s", json.dumps(record))
             yield record
 
 
